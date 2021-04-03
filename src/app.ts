e75fb7fcc68215641606aa9049fc9a5579e5ee34
Bryce McLachlan <brycemclachlan@gmail.com>
2021-04-03 18:12:30 +0000

    changed transcode endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ import indexRouter from './routes/index'
 
 const app = express()
 app.use(logger('dev'))
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use('/', indexRouter)
+app.use('/mp4', indexRouter)
 
 export default app
