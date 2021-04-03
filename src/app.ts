bd64888fd77d1cdea99729dc79e7be5ecb07e7f2
Bryce McLachlan <brycemclachlan@gmail.com>
2021-04-03 07:18:12 +0000

    added route for ffmpeg

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
+import indexRouter from './routes/index'
+
 const app = express()
 app.use(logger('dev'))
-app.use(express.json())
+// app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-
-import indexRouter from './routes/index'
 app.use('/', indexRouter)
 
 export default app
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,37 @@
 import express from 'express'
+import axios from 'axios'
+import ffmpeg from 'fluent-ffmpeg'
 const router = express.Router()
 
-router.get('/', function (_req: any, res: any, _next: any) {
-  res.send({ title: 'Express' })
-})
+const mp4 = async ({ res, input }: any) => {
+  const data = await axios({
+    method: 'get',
+    url: input,
+    responseType: 'stream',
+  }).then((response) => {
+    return response.data
+  })
 
+  ffmpeg()
+    .input(data)
+    .addOutputOption('-movflags', 'frag_keyframe+empty_moov')
+    .format('mp4')
+    .on('start', function (commandLine) {
+      console.log('Spawned Ffmpeg with command: ' + commandLine)
+    })
+    .on('end', function () {
+      console.log('file has been converted successfully')
+    })
+    .on('error', function (err) {
+      console.log('an error happened: ' + err)
+    })
+    .pipe(res, { end: true })
+}
+
+router.get('/:urlEncoded', function (req: any, res: any, _next: any) {
+  const url = decodeURIComponent(req.params.urlEncoded)
+  console.log('test')
+  res.contentType('video/mp4')
+  mp4({ res, input: url })
+})
 export default router
